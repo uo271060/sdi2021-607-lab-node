f471a4a7d22a0a25d4704fa6f4e6cc3cfd3ab345
uo271060 <uo271060@uniovi.es>
2021-03-22 14:34:57 +0100

    SDI-2021-607-7.1-Creación del proyecto Tienda Virtual.

---
+++ hello-world-server.js
@@ -0,0 +1,6 @@
+let http = require('http');
+http.createServer(function handler(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(8080, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:8080/');
