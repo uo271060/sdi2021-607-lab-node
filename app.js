4d698ef28a23a12e8a3b3b49700137e594e322ca
uo271060 <uo271060@uniovi.es>
2021-04-03 20:47:11 +0200

    SDI-2021-607-8.5-Colecciones relacionadas y routers.

--- app.js
+++ app.js
@@ -8,21 +8,48 @@ let crypto = require('crypto');
 let expressSession = require('express-session');
 let app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(fileUpload());
-app.set('clave', 'abcdefg');
-app.set('crypto', crypto);
-
-app.set('port', 8081);
-app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00.sixp2.mongodb.net:27017,tiendamusica-shard-00-01.sixp2.mongodb.net:27017,tiendamusica-shard-00-02.sixp2.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-wt3ec4-shard-0&authSource=admin&retryWrites=true&w=majority');
 app.use(expressSession({
     secret: 'abcdefg',
     resave: true,
     saveUninitialized: true
 }));
+let routerUsuarioSession = express.Router();
+routerUsuarioSession.use(function (req, res, next) {
+    console.log("routerUsuarioSession");
+    if (req.session.usuario) {
+        next();
+    } else {
+        console.log("va a : " + req.session.destino)
+        res.redirect("/identificarse");
+    }
+});
+app.use("/canciones/agregar", routerUsuarioSession);
+app.use("/publicaciones", routerUsuarioSession);
+let routerAudios = express.Router();
+routerAudios.use(function (req, res, next) {
+    console.log("routerAudios");
+    let path = require('path');
+    let idCancion = path.basename(req.originalUrl, '.mp3');
+    gestorBD.obtenerCanciones(
+        { "_id": mongo.ObjectID(idCancion) }, function (canciones) {
+            if (req.session.usuario && canciones[0].autor == req.session.usuario) {
+                next();
+            } else {
+                res.redirect("/tienda");
+            }
+        })
+});
+app.use("/audios/", routerAudios);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(fileUpload());
 
+
+app.set('clave', 'abcdefg');
+app.set('crypto', crypto);
+app.set('port', 8081);
+app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00.sixp2.mongodb.net:27017,tiendamusica-shard-00-01.sixp2.mongodb.net:27017,tiendamusica-shard-00-02.sixp2.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-wt3ec4-shard-0&authSource=admin&retryWrites=true&w=majority');
 
 gestorBD.init(app, mongo);
 
