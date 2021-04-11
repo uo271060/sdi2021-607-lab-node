20a30ca340ea9741ee8256c9886ae0a410a36d89
uo271060 <uo271060@uniovi.es>
2021-04-11 16:50:21 +0200

    SDI-2021-607-9.5-Sistema de paginación.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -210,14 +210,29 @@ module.exports = function (app, swig, gestorBD) {
         if (req.query.busqueda != null) {
             criterio = { "nombre": { $regex: ".*" + req.query.busqueda + ".*" } };
         }
-        gestorBD.obtenerCanciones(criterio, function (canciones) {
+        let pg = parseInt(req.query.pg);
+        if (req.query.pg == null) {
+            pg = 1;
+        }
+        gestorBD.obtenerCancionesPg(criterio, pg, function (canciones, total) {
             if (canciones == null) {
                 res.send("Error al listar ");
             } else {
-                let respuesta = swig.renderFile('views/btienda.html',
-                    {
-                        canciones: canciones
-                    });
+                let ultimaPg = total / 4;
+                if (total % 4 > 0) { // Sobran decimales
+                    ultimaPg = ultimaPg + 1;
+                }
+                let paginas = []; // paginas mostrar
+                for (let i = pg - 2; i <= pg + 2; i++) {
+                    if (i > 0 && i <= ultimaPg) {
+                        paginas.push(i);
+                    }
+                }
+                let respuesta = swig.renderFile('views/btienda.html', {
+                    canciones: canciones,
+                    paginas: paginas,
+                    actual: pg
+                });
                 res.send(respuesta);
             }
         });
